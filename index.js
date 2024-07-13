b9cd34203846dbb967d9f10992720761015b5425
Damdadira <suk971029@gmail.com>
2024-07-13 18:32:40 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb+srv://damdadi:1234@cluster0.e0xujcy.mongodb.net/')
+    .then(() => console.log('MongoDB Connected...'))
+    .catch((err) => console.log(err));
+
 app.get('/', (req, res) => res.send('hi'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
