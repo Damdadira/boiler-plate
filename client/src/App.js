64fc43d059292e695a15314fd45d518593bd6f61
Damdadira <suk971029@gmail.com>
2024-07-21 18:19:16 +0900

    randing, login, register page 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
     return (
         <Router>
             <div>
-                <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/dashboard'>Dashboard</Link>
-                    </li>
-                </ul>
-
-                <hr />
-
                 <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/dashboard' element={<Dashboard />} />
+                    <Route exact path='/' element={<LandingPage />} />
+                    <Route exact path='/login' element={<LoginPage />} />
+                    <Route exact path='/register' element={<RegisterPage />} />
                 </Routes>
             </div>
         </Router>
-    );
-}
-
-function Home() {
-    return (
-        <div>
-            <h2>Home 안녕하세요~</h2>
-        </div>
-    );
-}
-
-function About() {
-    return (
-        <div>
-            <h2>About 어바웃 페이지</h2>
-        </div>
-    );
-}
-
-function Dashboard() {
-    return (
-        <div>
-            <h2>Dashboard</h2>
-        </div>
     );
 }
 
