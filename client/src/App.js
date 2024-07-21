f13f8d55551bf109bad0e654a8d8af33c33720a7
Damdadira <suk971029@gmail.com>
2024-07-21 18:12:47 +0900

    react-router-dom 설치 후 페이지 이동 예시 작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/dashboard'>Dashboard</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/dashboard' element={<Dashboard />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+function Home() {
+    return (
+        <div>
+            <h2>Home 안녕하세요~</h2>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h2>About 어바웃 페이지</h2>
+        </div>
+    );
+}
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+        </div>
+    );
 }
 
 export default App;
